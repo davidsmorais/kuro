d00a69390ea1497785f91f675d01e34bd8eeca9b
David Morais <david@dsmorais.com>
2022-01-19 10:38:34 +0000

    🐞fix: sign in automatically

--- src/url.js
+++ src/url.js
@@ -1,15 +1,17 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  app: 'https://todo.microsoft.com/?app',
-  community: 'https://gitter.im/klaussinani/ao',
-  homepage: 'https://klaussinani.github.io/ao',
-  issue: 'https://github.com/klaussinani/ao/issues/new',
-  keyboardShortcutsRef: 'https://github.com/klaussinani/ao#keyboard-shortcuts',
-  license: 'https://github.com/klaussinani/ao/blob/master/license.md',
-  release: 'https://github.com/klaussinani/ao/releases/latest',
-  search: 'https://github.com/search?q=+is:issue+repo:klaussinani/ao',
-  searchFeatureRequests: 'https://github.com/klaussinani/ao/labels/feature-request',
-  source: 'https://github.com/klaussinani/ao',
-  update: 'https://raw.githubusercontent.com/klaussinani/ao/master/docs/update.json'
+  app: "https://to-do.live.com/tasks/",
+  community: "https://gitter.im/klaussinani/ao",
+  homepage: "https://klaussinani.github.io/ao",
+  issue: "https://github.com/klaussinani/ao/issues/new",
+  keyboardShortcutsRef: "https://github.com/klaussinani/ao#keyboard-shortcuts",
+  license: "https://github.com/klaussinani/ao/blob/master/license.md",
+  release: "https://github.com/klaussinani/ao/releases/latest",
+  search: "https://github.com/search?q=+is:issue+repo:klaussinani/ao",
+  searchFeatureRequests:
+    "https://github.com/klaussinani/ao/labels/feature-request",
+  source: "https://github.com/klaussinani/ao",
+  update:
+    "https://raw.githubusercontent.com/klaussinani/ao/master/docs/update.json"
 };
