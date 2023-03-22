2994a2663479b171aa033bd5886f889a156e7676
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2023-03-22 09:08:49 +0900

    Improve Nix package and sync to latest version & Nix Flake support! (#93)
    * nix: Improve Nix package and sync to latest version
    
    Improve suggestion from https://github.com/NixOS/nixpkgs/pull/211022
    
    * nix: flake support

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
 # TODO: add shell support (with electron and node_modules symlink)
-pkgs.callPackage ./release.nix { }
+pkgs.callPackage ./release.nix {
+  electron = pkgs.electron_22;
+}
 
